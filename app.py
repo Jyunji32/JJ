ca08544f8282ba977ee73ef7d49575fb4ded278a
Jyunji32 <2mnd964db4@privaterelay.appleid.com>
2026-03-29 00:51:15 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 import pandas as pd
 
-st.title("💪 筋トレ自己ベスト・チェッカー")
+st.title("💪筋トレ自己ベストチェッカー")
 
 # app.py の接続部分をこう書き換えてみる
 conn = st.connection("gsheets", type=GSheetsConnection)
